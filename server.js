69088975109dca35fc16203553e46d0ef5a42f50
ekater1na <ekaterina_l@tut.by>
2020-04-13 00:20:42 +0300

    fix: use linter

--- server.js
+++ server.js
@@ -7,17 +7,26 @@ const bodyparser = require('body-parser');
 
 const employeeController = require('./controllers/employeeController');
 
-var app = express();
-app.use(bodyparser.urlencoded({
+const app = express();
+app.use(
+  bodyparser.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(bodyparser.json());
 app.set('views', path.join(__dirname, '/views/'));
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    defaultLayout: 'mainLayout',
+    layoutsDir: `${__dirname}/views/layouts/`
+  })
+);
 app.set('view engine', 'hbs');
 
 app.listen(3000, () => {
-    console.log('Express server started at port : 3000');
+  console.log('Express server started at port : 3000');
 });
 
 app.use('/employee', employeeController);
