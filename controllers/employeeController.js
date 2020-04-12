146b9caebd953f280455e5d4dd0ca95060e8924a
ekater1na <ekaterina_l@tut.by>
2020-04-12 23:03:34 +0300

    feat: add user posting

--- controllers/employeeController.js
+++ controllers/employeeController.js
@@ -1,10 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const Employee = mongoose.model('Employee');
 
 router.get('/', (req, res) => {
     res.render("employee/addOredit", { 
         viewTitle: 'Insert Employee'
     })
+});
+
+router.post('/', (req, res) => {
+    insertRecord(req, res);
+});
+
+function insertRecord(req, res) {
+    let employee = new Employee();
+    employee.fullName = req.body.fullName;
+    employee.email = req.body.email;
+    employee.mobile = req.body.mobile;
+    employee.city = req.body.city;
+    employee.save((err, doc) => {
+        if (!err)
+            res.redirect('employee/list');
+        else {
+            if (err.name == 'ValidationError') {
+                handleValidationError(err, req.body);
+                res.render("employee/addOrEdit", {
+                    viewTitle: "Insert Employee",
+                    employee: req.body
+                });
+            }
+            else
+                console.log('Error during record insertion : ' + err);
+        }
+    });
+}
+
+router.get('/list', (req, res) => {
+    res.json('from list')
 });
 
 module.exports = router;
