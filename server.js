3f014947e04dca2e986ce0295ac874522b85a09f
ekater1na <ekaterina_l@tut.by>
2020-04-12 22:32:07 +0300

    feat: add views

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const employeeController = require('./controllers/employeeController');
 
 const app = express();
 
-app.set('views', path.join(__dirname, '/views'));
-app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + 'views/layouts'}));
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
 app.listen(3000, () => {
